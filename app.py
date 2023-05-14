6518f051d3a6a27f429c506b08b3985bd938211f
Lyon98.dbios <62766374+lyon98-dbios@users.noreply.github.com>
2023-05-14 15:24:34 +0100

    .

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from functools import wraps
+from werkzeug.utils import secure_filename
+import sqlite3
+import threading
+import os
+
+app = Flask(__name__, template_folder='templates')
+
+app.secret_key = "secret_key"
+
+# Check if user is logged in
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'username' not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+# Homepage
+@app.route('/')
+@login_required
+def admin_index():
+    return render_template('admin_index.html')
+
+@app.route('/')
+@login_required
+def user_index():
+    return render_template('user_index.html')
+
+# Login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if 'otp' in request.form:
+            # Handle OTP submission
+            otp = request.form['otp']
+            # Process OTP here
+            stored_otp = query_db(otp)
+            if stored_otp is not None and stored_otp == otp:
+                return redirect(url_for('registeration'))
+            else:
+                flash('Invalid Otp')
+        else:
+            # Handle username and password submission
+            username = request.form['username']
+            password = request.form['password']
+            stored_password = query_db(username)
+            # Process username and password here
+            if username == 'admin' and password == 'admin':
+                session['username'] = username
+                return redirect(url_for('admin_index')) 
+            elif stored_password is not None and stored_password == password:
+                session['username'] = username
+                return redirect(url_for('user_index'))                
+            else:
+                flash('Invalid username or password')
+    # Display login form
+    return render_template('login.html')
